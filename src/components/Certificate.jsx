03d6ce9baef07263c33a0731c729a223aa53c340
Mohitverve <155957883+Mohitverve@users.noreply.github.com>
2025-08-02 22:35:21 +0530

    cer

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import '../styles/certificate.css';  // global CSS
+import '../styles/certificate.css';
+import certImg from '../assets/1.png';
+import { Button } from '@mui/material';
 
-export default function Certificate({ imageSrc }) {
+export default function Certificate() {
   return (
     <div className="certificate">
       <img
-        src={imageSrc}
+        src={certImg}
         alt="Marriage Certificate"
         className="certificateImage"
       />
+      <Button
+        variant="contained"
+        sx={{ mt: 2 }}
+        component="a"
+        href={certImg}
+        download="certificate.png"
+      >
+        Download Certificate
+      </Button>
     </div>
   );
 }
